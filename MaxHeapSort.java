c0da67a39fd906477ec5b098b8e7aa61bd1f60c3
Vaishnavi  Shivaji  Mahadik <143684193+VaishnaviMahadik23@users.noreply.github.com>
2025-05-18 14:19:06 +0530

    MaxHeapSort Program

---
+++ MaxHeapSort.java
@@ -0,0 +1,65 @@
+
+import java.util.Scanner;
+
+public class MaxHeapSort {
+
+    static void HeapSort(int arr[],int n){
+        for(int i=(n/2)-1;i>=0;i--){
+            heapify(arr,n,i);
+        }
+
+        for(int i=n-1;i>0;i--){
+            int temp=arr[0];
+            arr[0]=arr[i];
+            arr[i]=temp;
+            heapify(arr,i,0);
+        }
+
+    }
+
+    static void heapify(int arr[] ,int n,int i){
+
+        int largest=i;
+        int left=2*i+1;
+        int right=2*i+2;
+
+        if(left<n && arr[left]>arr[largest]){
+            largest=left;
+        }
+
+        if(right<n && arr[right]>arr[largest]){
+            largest=right;
+        }
+
+        if(largest!=i){
+            int temp=arr[i];
+            arr[i]=arr[largest];
+            arr[largest]=temp;
+            heapify(arr,n,largest);
+        }
+
+    }
+
+    
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+
+        System.out.println("Enter size of array:");
+        int n=s.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of array");
+        for(int i=0;i<n;i++){
+           arr[i]=s.nextInt();
+        }
+ 
+        HeapSort(arr,n);
+        System.out.println("Sorted Array is:");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+
+    }
+}
